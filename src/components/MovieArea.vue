e1033d6cbbe422eadf056181fd58edada09d5917
DaviLeoMeda <davileo.meda@gmail.com>
2023-05-11 13:33:45 +0200

    Step Three - Axios activated

--- src/components/MovieArea.vue
+++ src/components/MovieArea.vue
@@ -1,6 +1,7 @@
 <script>
 import MovieCard from './MovieCard.vue'
 import { store } from "../store"
+import axios from 'axios'
 
 export default {
     name: "MovieArea",
@@ -16,16 +17,22 @@ export default {
     crerated() {
 
     },
-    methods() {
+    methods: {
 
+        APIcall() {
+            axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${store.api_key}&query=${store.searchText}`)
+                .then((res) => {
+                    store.arrayMovie = res.data.results
+                })
+        }
     }
 }
 </script>
 
 <template>
     <div class="inserting d-flex p-5">
-        <input type="text" placeholder="Select your favorite Movie or Serie">
-        <button>Send</button>
+        <input type="text" placeholder="Select your favorite Movie or Serie" v-model="store.searchText">
+        <button @click="APIcall">Search it!</button>
     </div>
 
     <MovieCard />
--- src/store.js
+++ src/store.js
@@ -2,6 +2,8 @@ import { reactive } from "vue";
 
 export const store = reactive(
     {
-
+        searchText: '',
+        arrayMovie: [],
+        api_key: 'b29ee27e1e73a14a1a80ea85ceeaa9d6'
     }
 )
